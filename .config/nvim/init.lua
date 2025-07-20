9e1289167009ee8a15cf61a856c264806102d397
Kyedo <31018128+itskyedo@users.noreply.github.com>
2025-07-19 20:01:38 -0700

    nvim: remove vim-tmux-navigator

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -491,25 +491,6 @@ lazy.setup({
   },
 
   {
-    "christoomey/vim-tmux-navigator",
-    event = "VeryLazy",
-    cmd = {
-      "TmuxNavigateLeft",
-      "TmuxNavigateDown",
-      "TmuxNavigateUp",
-      "TmuxNavigateRight",
-      "TmuxNavigatePrevious",
-    },
-    keys = {
-      { "<C-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
-      { "<C-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
-      { "<C-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
-      { "<C-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
-      { "<C-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-    },
-  },
-
-  {
     "shatur/neovim-session-manager",
     lazy = false,
     dependencies = { "nvim-lua/plenary.nvim" },
